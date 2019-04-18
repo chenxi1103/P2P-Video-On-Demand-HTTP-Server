792111397561f968da300215397f6d3ea93f8539
Chenxi Li <chenxili@andrew.cmu.edu>
2019-04-18 00:04:27 -0400

    range request ok

--- src/main/java/HttpServer.java
+++ src/main/java/HttpServer.java
@@ -8,7 +8,7 @@ import java.util.concurrent.Executor;
 import java.util.concurrent.Executors;
 
 public class HttpServer implements Runnable {
-    static final int PORT = 10006;
+    static final int PORT = 10009;
     private Socket socket;
     private String serverTime;
     private String parentPath = "src/main/resources/";
@@ -18,11 +18,12 @@ public class HttpServer implements Runnable {
     private OutputStream outputStream = null;
     private PrintWriter printWriter = null;
     private boolean rangeRequest;
+    private boolean fileNotFound;
     private String range;
     private byte[] byteFile;
     private BufferedOutputStream bufferedOutputStream = null;
     // Use thread pool for better thread management
-    private static final Executor exec = Executors.newCachedThreadPool();
+    private static final Executor threadPool = Executors.newFixedThreadPool(50);
 
     /**
      * Constructor of the HttpServer
@@ -43,7 +44,7 @@ public class HttpServer implements Runnable {
             ServerSocket serverSocket = new ServerSocket(PORT);
             while (true) {
                 HttpServer server = new HttpServer(serverSocket.accept());
-                exec.execute(server);
+                threadPool.execute(server);
             }
 
         } catch (IOException e) {
@@ -85,12 +86,13 @@ public class HttpServer implements Runnable {
 
                 String requestHeader = "";
                 while ((requestHeader = bufferedReader.readLine()).length() != 0) {
-                    System.out.println(requestHeader);
+//                    System.out.println(requestHeader);
                     if (requestHeader.split(":")[0].toLowerCase().equals("range")) {
                         this.rangeRequest = true;
                         this.range = requestHeader.split(":")[1].split("=")[1];
                     }
                 }
+
 
                 // If this is a GET request
                 if (method.toUpperCase().equals("GET")) {
@@ -101,101 +103,127 @@ public class HttpServer implements Runnable {
                     File file = new File(parentPath, requestFile);
                     byteFile = getFileByte(file);
 
-                    if (this.rangeRequest) {
-                        int start, end = 0;
-                        start = Integer.parseInt(this.range.split("-")[0]);
-                        if (this.range.split("-").length == 1) {
-                            end = byteFile.length;
-                        } else {
-                            end = Integer.parseInt(this.range.split("-")[1]) + 1;
+                    if (this.fileNotFound) {
+                        requestFile = "404.html";
+                        File notFoundFile = new File(parentPath, requestFile);
+                        byte[] bytefile = getFileByte(notFoundFile);
+                        printWriter.println("HTTP/1.0 404 Not Found");
+                        // Generate Server Time in RFC 1123-format
+                        serverTime =
+                                DateTimeFormatter.RFC_1123_DATE_TIME.format(ZonedDateTime.now(ZoneOffset.UTC));
+                        printWriter.println("Date: " + serverTime);
+                        String contentType = "text/html";
+                        printWriter.println("Content-Type: " + contentType);
+                        String contentLength =
+                                Integer.toString((int) notFoundFile.length());
+                        printWriter.println("Content-Length: " + contentLength);
+                        // Empty line -> indicate the end of the header field
+                        printWriter.println("");
+                        printWriter.flush();
+                        try {
+                            bufferedOutputStream.write(bytefile);
+                            bufferedOutputStream.flush();
+                        } catch (IOException e2) {
+                            System.out.println("404 error: " + e2.getMessage());
                         }
-                        byteFile = Arrays.copyOfRange(byteFile, start, end);
-                        printWriter.println("HTTP/1.0 206 Partial content");
                     } else {
-                        printWriter.println("HTTP/1.0 200 OK");
-                    }
+                        if (this.rangeRequest) {
+                            int start, end = 0;
+                            start = Integer.parseInt(this.range.split("-")[0]);
+                            if (this.range.split("-").length == 1) {
+                                end = byteFile.length;
+                            } else {
+                                end = Integer.parseInt(this.range.split("-")[1]) + 1;
+                            }
+                            byteFile = Arrays.copyOfRange(byteFile, start, end);
+                            this.range = start + "-" + (end - 1);
+                            printWriter.println("HTTP/1.0 206 Partial content");
+                        } else {
+                            printWriter.println("HTTP/1.0 200 OK");
+                        }
+                        // Generate Server Time in RFC 1123-format
+                        serverTime =
+                                DateTimeFormatter.RFC_1123_DATE_TIME.format(ZonedDateTime.now(ZoneOffset.UTC));
+                        printWriter.println("Date: " + serverTime);
+                        String suffix =
+                                requestFile.substring(requestFile.lastIndexOf(".") + 1).toLowerCase();
 
-                    // Generate Server Time in RFC 1123-format
-                    serverTime =
-                            DateTimeFormatter.RFC_1123_DATE_TIME.format(ZonedDateTime.now(ZoneOffset.UTC));
-                    printWriter.println("Date: " + serverTime);
-                    String suffix =
-                            requestFile.substring(requestFile.lastIndexOf(".") + 1).toLowerCase();
+                        // Parse the request file type
+                        String contentType = "";
+                        switch (suffix) {
+                            case "txt":
+                                contentType = "text/plain";
+                                break;
+                            case "css":
+                                contentType = "text/css";
+                                break;
+                            case "gif":
+                                contentType = "image/gif";
+                                break;
+                            case "jpg":
+                            case "jpeg":
+                                contentType = "image/jpeg";
+                                break;
+                            case "png":
+                                contentType = "image/png";
+                                break;
+                            case "js":
+                                contentType = "application/javascript";
+                                break;
+                            case "html":
+                            case "htm":
+                                contentType = "text/html";
+                                break;
+                            case "mp4":
+                                contentType = "video/mp4";
+                                break;
+                            case "webm":
+                                contentType = "video/webm";
+                                break;
+                            case "ogg":
+                                contentType = "video/ogg";
+                                break;
+                            default:
+                                contentType = "application/octet-stream";
+                        }
 
-                    // Parse the request file type
-                    String contentType = "";
-                    switch (suffix) {
-                        case "txt":
-                            contentType = "text/plain";
-                            break;
-                        case "css":
-                            contentType = "text/css";
-                            break;
-                        case "gif":
-                            contentType = "image/gif";
-                            break;
-                        case "jpg":
-                        case "jpeg":
-                            contentType = "image/jpeg";
-                            break;
-                        case "png":
-                            contentType = "image/png";
-                            break;
-                        case "js":
-                            contentType = "application/javascript";
-                            break;
-                        case "html":
-                        case "htm":
-                            contentType = "text/html";
-                            break;
-                        case "mp4":
-                            contentType = "video/mp4";
-                            break;
-                        case "webm":
-                            contentType = "video/webm";
-                            break;
-                        case "ogg":
-                            contentType = "video/ogg";
-                            break;
-                        default:
-                            contentType = "application/octet-stream";
+                        String contentLength = Integer.toString((int) file.length());
+                        if (this.rangeRequest) {
+                            printWriter.println("Content-Range: bytes " + this.range + "/" + contentLength);
+                            contentLength = Integer.toString(byteFile.length);
+                        }
+                        printWriter.println("Content-Length: " + contentLength);
+                        printWriter.println("Content-Type: " + contentType);
+                        // Empty line -> indicate the end of the header field
+                        printWriter.println("");
+                        printWriter.flush();
+                        bufferedOutputStream.write(byteFile);
+                        bufferedOutputStream.flush();
                     }
-                    String contentLength = Integer.toString((int) file.length());
-                    if (this.rangeRequest) {
-                        printWriter.println("Content-Range: bytes" + this.range + "/" + contentLength);
-                        contentLength = Integer.toString(byteFile.length);
-                    }
-                    printWriter.println("Content-Length: " + contentLength);
-                    printWriter.println("Content-Type: " + contentType);
-                    // Empty line -> indicate the end of the header field
-                    printWriter.println("");
-                    printWriter.flush();
-                    bufferedOutputStream.write(byteFile);
-                    bufferedOutputStream.flush();
                 }
             }
         } catch (IOException e1) {
             System.out.println("General error: " + e1.getMessage());
-        } finally {
-            try {
-                if (inputStream != null) {
-                    inputStream.close();
-                }
-                if (bufferedReader != null) {
-                    bufferedReader.close();
-                }
-                if (outputStream != null) {
-                    outputStream.close();
-                }
-                if (printWriter != null) {
-                    printWriter.close();
-                }
-                if (bufferedOutputStream != null) {
-                    bufferedOutputStream.close();
-                }
-            } catch (IOException e) {
-                System.out.println("Finally error: " + e.getMessage());
-            }
+        }finally {
+//            try {
+//                if (bufferedReader != null) {
+//                    bufferedReader.close();
+//                }
+//                if (printWriter != null) {
+//                    printWriter.close();
+//                }
+//                if (bufferedOutputStream != null) {
+//                    bufferedOutputStream.close();
+//                }
+//                if (inputStream != null) {
+//                    inputStream.close();
+//                }
+//                if (outputStream != null) {
+//                    outputStream.close();
+//                }
+//            } catch (IOException e) {
+//                e.printStackTrace();
+//            }
         }
     }
 
@@ -212,37 +240,9 @@ public class HttpServer implements Runnable {
             fileInputStream.read(byteFile);
             fileInputStream.close();
         } catch (FileNotFoundException e) {
-            requestFile = "404.html";
-            File notFoundFile = new File(parentPath, requestFile);
-            byte[] bytefile = getFileByte(notFoundFile);
-            printWriter.println("HTTP/1.0 404 Not Found");
-            // Generate Server Time in RFC 1123-format
-            serverTime =
-                    DateTimeFormatter.RFC_1123_DATE_TIME.format(ZonedDateTime.now(ZoneOffset.UTC));
-            printWriter.println("Date: " + serverTime);
-            String contentType = "text/html";
-            printWriter.println("Content-Type: " + contentType);
-            String contentLength =
-                    Integer.toString((int) notFoundFile.length());
-            printWriter.println("Content-Length: " + contentLength);
-            // Empty line -> indicate the end of the header field
-            printWriter.println("");
-            printWriter.flush();
-            try {
-                bufferedOutputStream.write(bytefile);
-                bufferedOutputStream.flush();
-            } catch (IOException e2) {
-                System.out.println("404 error: " + e2.getMessage());
-            }
+            this.fileNotFound = true;
         } catch (IOException e1) {
             System.out.println("getFileByte error: " + e1.getMessage());
-        } finally {
-            try {
-                printWriter.close();
-                bufferedOutputStream.close();
-            } catch (IOException e) {
-                System.out.println("404 Finally error: " + e.getMessage());
-            }
         }
         return byteFile;
     }
